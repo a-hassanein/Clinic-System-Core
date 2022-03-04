fbb50576d222b7fff14488c3da56c6f1fb36e6d4
a-hassanein <ahassaneina7@gmail.com>
2022-03-04 19:36:20 +0200

    edit models

--- clinic/main/models.py
+++ clinic/main/models.py
@@ -2,20 +2,23 @@ from django.db import models
 
 # Create your models here.
 
-
-class Clinic(models.Model):
-    clinic_id = models.AutoField(primary_key=True)
-    clinic_name = models.CharField(max_length=200, null=False)
-    clinic_address = models.CharField(max_length=500)
-
-
 class Doctor(models.Model):
     doctor_id = models.AutoField(primary_key=True)
-    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
     doctor_name = models.CharField(max_length=200, null=False)
     doctor_gender = models.CharField(max_length=20, null=False)
     doctor_email = models.EmailField(max_length=100, null=False)
     doctor_age = models.IntegerField(null=False)
+    doctor_password = models.CharField(null=False, max_length=300)
+    doctor_confirm_password = models.CharField(null=False, max_length=300)
+
+
+
+class Clinic(models.Model):
+    clinic_id = models.AutoField(primary_key=True)
+    doctor_id = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    clinic_name = models.CharField(max_length=200, null=False)
+    clinic_address = models.CharField(max_length=500)
+
 
 
 class Assistant(models.Model):
@@ -29,9 +32,9 @@ class Assistant(models.Model):
 
 
 class Patient(models.Model):
-    patient_phone = models.CharField(max_length=20, primary_key=True)
+    patient_id = models.AutoField(primary_key=True)
+    patient_phone = models.CharField(max_length=20, unique=True)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    assistant = models.ForeignKey(Assistant, on_delete=models.CASCADE)
     patient_name = models.CharField(max_length=200, null=False)
     patient_gender = models.CharField(max_length=20, null=False)
     current_disease = models.CharField(max_length=1000)
@@ -44,21 +47,21 @@ class Appointment(models.Model):
     appointment_id = models.AutoField(primary_key=True)
     patient_name = models.CharField(max_length=100 , null=False)
     patient_phone = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    appointment_date = models.DateField()
+    appointment_date = models.DateTimeField(null=False)
     checkup_type = models.CharField(max_length=25, null=False)
     completed = models.BooleanField(default=False)
 
 
 class Labs(models.Model):
     Lab_id = models.AutoField(primary_key=True)
-    patient_phone = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    appointment_id = models.ForeignKey(Appointment, on_delete=models.CASCADE)
     Lab_name = models.CharField(max_length=100, null=False)
     Lab_type = models.CharField(max_length=50, null=False)
 
 
 class Prescription(models.Model):
     prescription_id = models.AutoField(primary_key=True)
-    patient_phone = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    appointment_id = models.ForeignKey(Appointment, on_delete=models.CASCADE)
     drug_name = models.CharField(max_length=200)
     drug_dose = models.CharField(max_length=100)
     drug_dosage_form = models.CharField(max_length=100)
@@ -70,7 +73,7 @@ class Prescription(models.Model):
 
 class Bill(models.Model):
     bill_id = models.AutoField(primary_key=True)
-    patient_phone = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    appointment_id = models.ForeignKey(Appointment, on_delete=models.CASCADE)
     activity_name = models.CharField(max_length=200, null=False)
     activity_price = models.IntegerField(null=False)
     total_price = models.IntegerField(null=True)
@@ -78,7 +81,6 @@ class Bill(models.Model):
 
 class Surgery(models.Model):
     surgery_id = models.AutoField(primary_key=True)
-    bill = models.ForeignKey(Bill, on_delete=models.CASCADE, null=True)
     surgery_name = models.CharField(max_length=100, null=False)
     surgery_price = models.IntegerField(null=False)
     surgery_description = models.CharField(max_length=100, null=False)
@@ -86,9 +88,8 @@ class Surgery(models.Model):
 
 class Material(models.Model):
     material_id = models.AutoField(primary_key=True)
-    bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
     material_name = models.CharField(max_length=100, null=False)
-    material_price = models.IntegerField( null=False)
+    material_price = models.IntegerField(null=False)
     material_usage = models.CharField(max_length=200, null=False)
 
 
