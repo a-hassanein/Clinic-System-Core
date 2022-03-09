da75016afb06a911d2345475d03241d27d3d6bc0
Ahmedxd199 <ahmedashraf012@fci.helwan.edu.eg>
2022-03-09 02:18:05 +0200

    setting

--- clinic/clinic/settings.py
+++ clinic/clinic/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'materials.apps.MaterialsConfig',
     'assistant.apps.AssistantConfig',
     'doctors.apps.DoctorsConfig',
+    'setting.apps.SettingConfig',
 
 ]
 # REST_FRAMEWORK = {
