daa30052b6ef42a7afe6aa40e33228e47435afc3
Abdelrahman <abdelrahmanmohamedkhaleel@gmail.com>
2022-03-10 20:15:18 +0200

    favourite

--- clinic/prescription/urls.py
+++ clinic/prescription/urls.py
@@ -2,8 +2,11 @@ from django.urls import include, path
 from rest_framework import routers
 from .views import *
 router = routers.DefaultRouter()
-router.register('prescription' , Prescriptionviewset )
+router.register(r'prescription' , Prescriptionviewset )
 urlpatterns = [
+    path('drugs/<id>', drugget),
+    path('drugdel/<id>', drugdelete),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
